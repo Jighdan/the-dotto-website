3a60d30be80d64c99ea9ecc1bf7588a02995513e
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2023-03-03 23:41:30 -0400

    feat: adding fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+      serif: ["Playfair Display", ...defaultTheme.fontFamily.serif],
+    },
+  },
+  plugins: [],
+};
