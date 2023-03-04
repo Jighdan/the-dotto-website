a55ae59d319a7fa73b19541eab9d4910686acaab
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2023-03-03 23:43:08 -0400

    feat: defining colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,18 @@ module.exports = {
       sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
       serif: ["Playfair Display", ...defaultTheme.fontFamily.serif],
     },
+
+		colors: {
+			black: '#0c0e10',
+			white: {
+				'01': '#fffeff',
+				'02': '#ebedd5'
+			},
+			green: {
+				'01': '#173300',
+				'02': '#9ee96f'
+			}
+		}
   },
   plugins: [],
 };
