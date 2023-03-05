63ebc0bb0e5652660877410a32b65cef5efcd2f5
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2023-03-05 17:22:36 -0400

    refactor: separating selectors

--- src/animations.ts
+++ src/animations.ts
@@ -1,14 +1,24 @@
 import { gsap } from 'gsap';
 
 window.addEventListener('DOMContentLoaded', () => {
+	const selectors = {
+		logoArrow: '#logo > line',
+		layoutHeaderTitle: '.layout-header > h3',
+		sectionHero: '.section-hero > *',
+		sectionContact: '.section-contact > *:not(div)',
+		sectionContactCta: '.section-contact > div',
+		layoutFooter: '.layout-footer > div',
+		layoutFooterFigure: '.layout-footer > figure',
+	}
+
 	const timeline = gsap.timeline({ defaults: { duration: 0.75, ease: 'power2.out' } });
 
 	timeline
-		.fromTo('#logo > line', { translateX: -100, }, { translateX: 0 })
-		.fromTo('.layout-header > h3', { opacity: 0 }, { opacity: 1 })
-		.fromTo('.section-hero > *', { opacity: 0, translateX: -100 }, { opacity: 1, translateX: 0, stagger: 0.5 })
-		.fromTo('.section-contact > *:not(div)', { opacity: 0, translateX: -100 }, { opacity: 1, translateX: 0, stagger: 0.5 })
-		.fromTo('.section-contact > div', { opacity: 0, translateY: 100 }, { opacity: 1, translateY: 0 })
-		.fromTo('.layout-footer > div', { opacity: 0}, { opacity: 1 })
-		.fromTo('.layout-footer > figure', { opacity: 0 }, { opacity: 1 })
+		.fromTo(selectors.logoArrow, { translateX: -100, }, { translateX: 0 })
+		.fromTo(selectors.layoutHeaderTitle, { opacity: 0 }, { opacity: 1 })
+		.fromTo(selectors.sectionHero, { opacity: 0, translateX: -100 }, { opacity: 1, translateX: 0, stagger: 0.5 })
+		.fromTo(selectors.sectionContact, { opacity: 0, translateX: -100 }, { opacity: 1, translateX: 0, stagger: 0.5 })
+		.fromTo(selectors.sectionContactCta, { opacity: 0, translateY: 100 }, { opacity: 1, translateY: 0 })
+		.fromTo(selectors.layoutFooter, { opacity: 0 }, { opacity: 1 })
+		.fromTo(selectors.layoutFooterFigure, { opacity: 0 }, { opacity: 1 })
 });
