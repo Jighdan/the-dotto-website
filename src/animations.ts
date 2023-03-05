b3df11f5c9316420c48fd7204f377ac172bc1163
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2023-03-05 17:20:07 -0400

    style: page animations

--- src/animations.ts
+++ src/animations.ts
@@ -6,4 +6,9 @@ window.addEventListener('DOMContentLoaded', () => {
 	timeline
 		.fromTo('#logo > line', { translateX: -100, }, { translateX: 0 })
 		.fromTo('.layout-header > h3', { opacity: 0 }, { opacity: 1 })
+		.fromTo('.section-hero > *', { opacity: 0, translateX: -100 }, { opacity: 1, translateX: 0, stagger: 0.5 })
+		.fromTo('.section-contact > *:not(div)', { opacity: 0, translateX: -100 }, { opacity: 1, translateX: 0, stagger: 0.5 })
+		.fromTo('.section-contact > div', { opacity: 0, translateY: 100 }, { opacity: 1, translateY: 0 })
+		.fromTo('.layout-footer > div', { opacity: 0}, { opacity: 1 })
+		.fromTo('.layout-footer > figure', { opacity: 0 }, { opacity: 1 })
 });
