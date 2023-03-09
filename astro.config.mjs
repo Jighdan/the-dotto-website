33a62258a165775db5ecac40321eeb096d81347a
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2023-03-09 00:12:13 -0400

    chore: deprecating use of analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
 
 export default defineConfig({
-  integrations: [tailwind(), partytown()]
+  integrations: [tailwind()]
 });
