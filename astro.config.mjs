5ac04bd87aa8fa2719c53b34c0a1e4b62ab80b6d
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2023-03-08 23:55:00 -0400

    fix: using @astro/vercel solution for analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel({
+    analytics: true,
+    excludeFiles: ['assets/logo.png']
+  })
 });
